c0528b0469376e364e4cfecb49fb77ab7b22184e
tomoya_edzuka <80945931+t-edzuka@users.noreply.github.com>
2022-03-20 22:44:19 +0900

    feat: division and multiplication features done

--- go/money_test.go
+++ go/money_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+type Multiplier interface {
+	Times()
+}
+
+type Divider interface {
+	divide()
+}
+
 type Dollar struct {
 	amount int
 }
@@ -20,4 +28,41 @@ func TestMultiplication(t *testing.T) {
 	if tenner.amount != 10 {
 		t.Errorf("Expected 10, got: [%d]", tenner.amount)
 	}
+}
+
+// End of Chapter1
+
+// Start of Chapter2
+type Money struct {
+	amount   float64
+	currency string
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{m.amount * float64(multiplier),
+		m.currency}
+}
+
+func (m Money) divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
+}
+
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %v Got %+v", expected, actual)
+	}
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"}
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualMoneyAfterDivision := originalMoney.divide(4)
+	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)
 }
